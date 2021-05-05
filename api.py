60550cfa5da423f34fc6db8b6cf50db2e0b2fbbf
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-05-05 22:36:56 +0200

    Patch api

--- api.py
+++ api.py
@@ -1,27 +1,25 @@
-from typing import ClassVar
 import json
 import requests
-hypixelUrl = "https://api.hypixel.net/"
-minecraftUrl = "https://api.mojang.com/"
 class API:
+    foundUUIDs = {}
+    foundPlayers = {}
+    
+    # TODO: Queue party and self for statistics refresh when requested
+
+    def __init__(self):
+        self.foundPlayers.setdefault(-1)
+        self.foundUUIDs.setdefault(-1)
+
     def hypixel(self, token, uuid):
-        if (not self.checkup()):
-            return -1
-        return requests.get(hypixelUrl + "player?key={}&uuid={}".format(token, uuid)).content
+        if (uuid in self.foundUUIDs):
+            return self.foundUUIDs[uuid]
+        player = requests.get("https://api.hypixel.net/player?key={}&uuid={}".format(token, uuid)).content
+        self.foundUUIDs[uuid] = player
+        return player
 
     def minecraft(self, username):
-        if (not self.checkup()):
-            return -1
-        return json.loads(requests.get(minecraftUrl + "users/profiles/minecraft/" + username).content)["id"]
-        
-    def checkup(self):
-        hypixel = requests.get(hypixelUrl)
-        minecraft = requests.get(minecraftUrl)
-        if (not hypixel):
-            print("Hypixel API server seems to be down")
-            return False
-        if (not minecraft):
-            print("Minecraft API server seems to be down")
-            return False
-        return True
-        
+        if (username in self.foundPlayers):
+            return self.foundPlayers[username]
+        uuid = json.loads(requests.get("https://api.mojang.com/users/profiles/minecraft/" + username).content)["id"]
+        self.foundPlayers[username] = uuid
+        return uuid
