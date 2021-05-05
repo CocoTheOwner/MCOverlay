f86b208b0058b4915e39911baf5aa8d96db06f1d
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-05-05 22:51:37 +0200

    Add default config, add log monitor, add main loop

--- MCO.py
+++ MCO.py
@@ -1,10 +1,36 @@
-from collections import defaultdict
+from api import API
+from logMonitor import logMonitor
 from config import Config
+import time
+
+# Default configuration
 defaultConfig = {
     "ownUsers": [
-        "username"
+        "cocodef9"
     ],
-    "token": "token"
+    "token": "token",
+    "logFolder": "C:\\Users\\sjoer\\Appdata\\Roaming\\Minecraft 1.8.9\\logs\\latest.log"
 }
+
+# Load configuration file (if empty, loads defaultConfig)
 config = Config('./config.json', defaultConfig)
+
+# Save the configuration file
 config.save()
+
+# Create a configuration file logger
+logger = logMonitor(config.get("logFolder"))
+
+# Create an API object
+api = API()
+
+def start():
+    for user in config.get("ownUsers"):
+        print("User: {} has UUID: {}".format(user, api.minecraft(user)))
+    while(True):
+        logger.tick()
+        time.sleep(0.1)
+
+
+if (__name__ == "__main__"):
+    start()
--- logMonitor.py
+++ logMonitor.py
@@ -1,6 +1,59 @@
+import os.path
+from types import prepare_class
 class logMonitor:
     path = ""
-    def __init__(self, path):
-        self.path = path
+    mtime = 0
+    linenumber = 0
+    debug = False
+    queue = []
 
-    
+    def __init__(self, path, debug = False):
+        self.path = path
+        self.debug = debug
+
+    def tick(self):
+        if (self.mtime != os.path.getmtime(self.path)):
+            self.mtime = os.path.getmtime(self.path)
+            self.readlog()
+
+    def readlog(self):
+        # Stores content of file
+        content = ""
+
+        # Retrieve content of file
+        with open(self.path, "r") as f:
+            content = f.read()
+
+        # Splits the content into lines
+        if (self.linenumber == 0):
+            content = content.splitlines()
+        else:
+            content = content.splitlines()[self.linenumber + 1:content.count("\n") + 1]
+        if len(content) != 0:
+
+            # Loops over all lines in reversed order
+            for line in content:
+
+                # Add one to linecounter
+                self.linenumber += 1
+
+                # Make sure line is useful (very lightweight)
+                if (not line.startswith("[")):
+                    continue
+
+                # (somewhat heavier)
+                if (line.find("[Client thread/INFO]: [CHAT]") == -1):
+                    continue
+
+                # (quite heavy)
+                info = line.strip().split("[Client thread/INFO]: [CHAT]")[1].strip()
+
+                # (very light)
+                if (info == ""):
+                    continue
+
+                # (quite heavy)
+                print("Line: " + info)
+
+                # (quite light)
+                self.queue.append(info)
