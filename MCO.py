ff17688891cbcc66fcb5348e36c3dc868fa989e8
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-05-07 21:19:06 +0200

    Clean up main file

--- MCO.py
+++ MCO.py
@@ -2,23 +2,16 @@ from api import API
 from logMonitor import logMonitor
 from config import Config
 import time
-from multiprocessing import Process, Lock
 
-# Default configuration
-defaultConfig = {
+# Load configuration file (if empty, loads defaultConfig)
+config = Config('./config/config.json', {
     "ownUsers": [
         "cocodef9"
     ],
-    "token": "token",
+    "token": "206baa63-dcd2-47f3-b197-b43de4e3301f",
     "logFolder": "C:\\Users\\sjoer\\Appdata\\Roaming\\Minecraft 1.8.9\\logs\\latest.log"
-}
-
-# Load configuration file (if empty, loads defaultConfig)
-config = Config('./config/config.json', defaultConfig)
+})
 players = Config('./config/players.json', {})
-
-# Save the configuration file
-config.save()
 
 # Create a configuration file logger
 logger = logMonitor(config.get("logFolder"), True)
@@ -26,33 +19,26 @@ logger = logMonitor(config.get("logFolder"), True)
 # Create an API object
 api = API(players.config, config.get("token"))
 
-# Reset log
-open('./config/log.txt', 'w').close()
-
-
-def start():
+# Main loop
+def startMCO():
     while(True):
+
+        # Update logger
         logger.tick()
-        if (logger.hasQueue):
-            for player in logger.getPlayers():
-                uuid = api.minecraft(player)
-                players.set(player, uuid)
-                stats = api.hypixel(uuid)
-                players.set(uuid, stats)
+
+        # Check for token update
+        if logger.newToken != None:
+            print("New token: " + logger.newToken)
+            api.token = logger.newToken
+            config.set("token", logger.newToken)
+            config.save()
+            logger.newToken = None
+
+        # Update player definitions
+        api.fetch(logger.getPlayers(), players)
+                
 
         time.sleep(0.1)
 
-
-
-def f(l, i):
-    l.acquire()
-    try:
-        print('hello world', i)
-    finally:
-        l.release()
-
 if __name__ == '__main__':
-    #lock = Lock()
-    start()
-    #for num in range(10):
-    #    Process(target=f, args=(lock, num)).start()
+    startMCO()
