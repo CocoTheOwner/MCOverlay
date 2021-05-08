70f741ce31ebe2525305c807dc5f294308b6151b
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-05-08 22:47:38 +0200

    Add back api request for Hypixel stats and show a success message

--- api.py
+++ api.py
@@ -82,11 +82,12 @@ class API:
         """
         #if (self.debug): print("Downloading stats of {} ({})".format(player, uuid))
         print("Downloading stats of {} ({})".format(player, uuid))
-        request = None #API.getRequest("https://api.hypixel.net/player?key={}&uuid={}".format(self.token, uuid))
+        request = API.getRequest("https://api.hypixel.net/player?key={}&uuid={}".format(self.token, uuid))
         if request == None or request["success"] != "true":
             self.players[uuid] = player
             return None
         else:
+            print("{}'s stats download successful".format(player))
             self.players[uuid] = request["player"]
             return request["player"]
 
